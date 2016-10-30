f0d282c23d6dd4684e9ad310133431b3b7a6c098
artomsk <arttatiana.omsk@gmail.com>
2016-10-30 16:44:46 +0300

    MVP
    xoxoxo

--- xo.js
+++ xo.js
@@ -1,1 +1,25 @@
-<script language="JavaScript"
+'use strict';
+
+function init() {
+    let turn = 0;
+    let boardElementsCollection = document.getElementsByClassName("bu");
+    let boardElements = Array.prototype.slice.call(boardElementsCollection);
+
+    boardElements.forEach(element => {
+        element.addEventListener("click", e => {
+            if (turn % 2 === 0) {
+                e.target.value = 'X';
+            } else {
+                e.target.value = 'O';
+            }
+
+            // e.target.classList.add('clickedP1');
+            // e.target.classList.add('clickedP2');
+            turn++;
+        })
+    });
+}
+
+window.onload = function() {
+    init();
+}
